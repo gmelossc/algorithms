b4bb1331a07c91303b958d993ffd8764d2be942f
Guilherme Melos <guilherme.melos@ufv.br>
2020-06-12 17:56:55 -0300

    bit-2d added

---
+++ data-structures/bit-2d.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 1e6+1;
+
+int bit[MAXN][MAXN], n;
+
+int sum(int x, int y){
+	int s = 0;
+
+	for(int i = x; i>0; i -= i & -i)
+		for(int j = y; j>0; j -= j & -j)
+			s += bit[i][j];
+
+	return s;
+}
+
+void update(int x, int y, int v){
+	for(int i = x; i < n; i += i & -i)
+		for(int j = y; i < n; j += j & -j)
+			bit[i][j] += v;
+}
