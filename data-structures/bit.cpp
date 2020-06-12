2487a91ea106a180c8cdd7b11224d8d3eeb26bd8
Guilherme Melos <guilherme.melos@ufv.br>
2020-06-12 13:45:22 -0300

    bit/fenwick-tree added

---
+++ data-structures/bit.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int MAX = 1e6 + 1;
+
+int bit[MAX], n;
+
+int sum(int x){
+  int s = 0;
+
+  for( ; x>0; x-=(x & -x)) s += bit[x];
+
+  return s;
+}
+
+void update(int x, int v){
+  for( ; x<=n; x+= (x & -x)) bit[x] += v;
+}
