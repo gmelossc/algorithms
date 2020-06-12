928151b46d81a9de5cf381ff40d7930607674597
Guilherme Melos <guilherme.melos@ufv.br>
2020-06-12 18:14:59 -0300

    adding sieve of eratosthenes

---
+++ math/sieve.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 1e6;
+
+vector<bool> sieve(MAX, true);
+
+void prepros(){
+	sieve[0] = sieve[1] = false;
+
+	for(int i = 2; i*i <= MAX; ++i){
+		if(sieve[i])
+			for(int j = i*i; j<=MAX; j+=i) sieve[j] = false;
+	}
+
+	// vi prime;
+	// for(int i = 2; i<=MAX; ++i) prime.push_back(i);
+	// return prime;
+}
