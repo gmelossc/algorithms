7f8270773745335fb4a0a0f9b7bf969880bb0f35
Guilherme Melos <guilherme.melos@ufv.br>
2020-07-01 19:25:00 -0300

    a

--- math/fast-exponenciation.cpp
+++ math/fast-exponenciation.cpp
@@ -7,16 +7,11 @@ const ll MOD = 1e9+7;
 ll mod(ll x, ll m){ if(x >= 0LL) return x % m; if(-x < m) return m-(-x); return mod(x%m, m);}
 
 ll fexp(int base, ll exp, ll m){
-	ll total = 1;
+	ll total = 1LL;
 	for(;exp ; exp>>=1LL){
 		if(exp & 1LL)
 			total = mod(((mod(total, m)) * (mod(base, m))), m);
 		base= mod((mod(base, m) * mod(base, m)), m);
 	}
 	return mod(total, m);
-}
-
-
-int main(){
-	cout<< fexp(2, 32, MOD)<< endl;
 }
