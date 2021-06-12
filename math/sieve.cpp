7aef5d7fbe2fd15b1f41728da4d222d7c01470e9
Dener Vieira <deneribeiro10@hotmail.com>
2021-06-12 12:29:55 -0300

    add sieve as struct

--- math/sieve.cpp
+++ math/sieve.cpp
@@ -19,3 +19,24 @@ void prepros(){
 		// if(sieve[i]) prime.push_back(i);
 	// return prime;
 }
+
+
+
+
+
+struct Sieve {
+    vector<int> div;
+    vector<int> primes;
+    Sieve(int N) {
+        div = vector<int>(N+1, 1);
+        for(int i=1; i<N; ++i) div[i] = i;
+        for(long long i=2; i<N; ++i) {
+            if(div[i] != i) continue;
+            primes.push_back(i);
+            for(long long j = i*i; j<N; j += i) {
+                div[j] = i;
+            }
+        }
+    }
+};
+const Sieve sieve(1'000'000);
