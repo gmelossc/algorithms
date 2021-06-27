c4d31c2f908de450dce5641a970ac45777b7ceea
Dener Vieira <deneribeiro10@hotmail.com>
2021-06-27 13:53:00 -0300

    make edmonds karp a structure

--- graph/edmonds-karp.cpp
+++ graph/edmonds-karp.cpp
@@ -2,72 +2,81 @@
 using namespace std;
 const int INF = 1e9;
 
-int n;
-vector<vector<int>> adj, capacity;
 
-int bfs(int source, int sink, vector<int> &parent) {
-    for(int &x : parent) x = -1;
-    parent[source] = 0;
-    queue<pair<int,int>> q;
-    q.push({source, INF});
+struct EdmondsKarp { 
+    int n, source, sink;
+    vector<vector<int>> adj, capacity;
 
-    while(!q.empty()) {
-        int curr = q.front().first;
-        int flow = q.front().second;
-        q.pop();
+    EdmondsKarp(int _n, int _source, int _sink) : n(_n), source(_source), sink(_sink) {
+        adj.assign(n, vector<int>());
+        capacity.assign(n, vector<int>(n));
+    };
 
-        for(auto to : adj[curr]) {
-            // not visited and has some remaining space
-            if(parent[to] == -1 and capacity[curr][to]) {
-                parent[to] = curr;
-                int new_flow = min(flow, capacity[curr][to]);
-                if(to == sink) {
-                    return new_flow;
+    void addEdge(int from, int to, int cap) {
+        adj[from].push_back(to);
+        capacity[from][to] += cap;
+    }
+
+    int bfs(vector<int> &parent) {
+        fill(parent.begin(), parent.end(), -1);
+        parent[source] = -2;
+        queue<pair<int,int>> q;
+        q.push({source, INF});
+
+        while(!q.empty()) {
+            int curr = q.front().first;
+            int flow = q.front().second;
+            q.pop();
+
+            for(auto to : adj[curr]) {
+                // not visited and has some remaining space
+                if(parent[to] == -1 and capacity[curr][to]) {
+                    parent[to] = curr;
+                    int new_flow = min(flow, capacity[curr][to]);
+                    if(to == sink) {
+                        return new_flow;
+                    }
+                    q.push({to, new_flow});
                 }
-                q.push({to, new_flow});
             }
         }
+        // sink never reached
+        return 0;
     }
-    // sink never reached
-    return 0;
-}
 
-int maxFlow(int source, int sink) {
-    int flow = 0;
-    vector<int> parent(n+1);
-    int new_flow = 0;
+    int maxFlow() {
+        int flow = 0;
+        vector<int> parent(n+1);
+        int new_flow = 0;
 
-    while(new_flow = bfs(source, sink, parent)) {
-        flow += new_flow;
-        int curr = sink;
-        while(curr != source) {
-            int prev = parent[curr];
-            capacity[prev][curr] -= new_flow;
-            capacity[curr][prev] += new_flow;
-            curr = prev;
+        while(new_flow = bfs(parent)) {
+            flow += new_flow;
+            int curr = sink;
+            while(curr != source) {
+                int prev = parent[curr];
+                capacity[prev][curr] -= new_flow;
+                capacity[curr][prev] += new_flow;
+                curr = prev;
+            }
         }
+        return flow;
     }
-
-    return flow;
-}
+};
 
 
 int main() {
 
-    cin >> n;
-    adj.assign(n+1, vector<int>());
-    capacity.assign(n+1, vector<int>(n+1));
+    EdmondsKarp graph(5, 0, 4);
+    
+    graph.addEdge(0, 1, 1);
+    graph.addEdge(0, 2, 2);
+    graph.addEdge(1, 2, 3);
+    graph.addEdge(1, 4, 2);
+    graph.addEdge(2, 3, 1);
+    graph.addEdge(3, 4, 3);
+    graph.addEdge(2, 1, 1);
 
-    int m;
-    cin >> m;
-    for(int i=0; i<m; ++i) {
-        int u, v, c;
-        cin >> u >> v >> c;
-        adj[u].push_back(v);
-        capacity[u][v] = c;
-    }
-
-    cout << maxFlow(1, n) << endl;
+    cout << graph.maxFlow() << endl;
 
     return 0;
 }
